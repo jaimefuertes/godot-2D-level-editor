4f3769df5147815b10e1374df3779d310db2c1bb
Jaime <72014992+jaimefuertes@users.noreply.github.com>
2024-01-15 20:26:46 +0100

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# 2D Level Editor - Godot 4
-A level editor for Godot 4.0.3 for ***platformer/metrodivania*** games
--- gle2D/addons/gle2d/components/TextEdit.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends TextEdit
-
-
-func _can_drop_data(at_position, data):
-	return typeof(data) == TYPE_DICTIONARY and data.has("files")
-
-func _drop_data(at_position, data):
-	text = data.files[0]
